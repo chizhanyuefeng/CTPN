6419760f51d0ad8a823737caecdc69b2da64d8a8
chizhanyuefeng <chizhanyuefeng@gmail.com>
2018-12-17 16:21:38 +0800

    完善proposal layer

--- lib/rpn_layer/proposal_layer_tf.py
+++ lib/rpn_layer/proposal_layer_tf.py
@@ -2,7 +2,6 @@
 import numpy as np
 from lib.rpn_layer.generate_anchors import generate_anchors
 
-
 from lib.utils.config import cfg
 from lib.bbox_utils.bbox_transform import bbox_transform_inv, clip_boxes
 from lib.nms.nms_wrapper import nms, py_cpu_nms
@@ -13,7 +12,7 @@ DEBUG = False
 Outputs object detection proposals by applying estimated bounding-box
 transformations to a set of regular boxes (called "anchors").
 """
-def proposal_layer(rpn_cls_prob_reshape, rpn_bbox_pred, im_info, cfg_key, _feat_stride = [16,], anchor_scales = [16,]):
+def proposal_layer(rpn_cls_prob_reshape, rpn_bbox_pred, im_info, _feat_stride=[cfg["ANCHOR_WIDTH"],]):
     """
     Parameters
     ----------
@@ -21,7 +20,6 @@ def proposal_layer(rpn_cls_prob_reshape, rpn_bbox_pred, im_info, cfg_key, _feat_
                          NOTICE: the old version is ordered by (1, H, W, 2, A) !!!!
     rpn_bbox_pred: (1 , H , W , Ax4), rgs boxes output of RPN
     im_info: a list of [image_height, image_width, scale_ratios]
-    cfg_key: 'TRAIN' or 'TEST'
     _feat_stride: the downsampling ratio of feature map to the original input image
     anchor_scales: the scales to the basic_anchor (basic anchor is [16, 16])
     ----------
@@ -44,8 +42,6 @@ def proposal_layer(rpn_cls_prob_reshape, rpn_bbox_pred, im_info, cfg_key, _feat_
     #layer_params = yaml.load(self.param_str_)
 
     """
-    cfg_key=cfg_key.decode('ascii')
-    # TODO 后期可能进行修改anchor的尺度,因为文本较为密集,需要进行完善修改
     # _anchors value
     # [[0    2   15   13]
     #  [0    0   15   15]
@@ -57,17 +53,17 @@ def proposal_layer(rpn_cls_prob_reshape, rpn_bbox_pred, im_info, cfg_key, _feat_
     #  [0  -62   15   77]
     #  [0  -91   15  106]
     #  [0 -134   15  149]]
-    _anchors = generate_anchors(scales=np.array(anchor_scales))#生成基本的10个anchor
+    _anchors = generate_anchors()#生成基本的10个anchor
     _num_anchors = _anchors.shape[0]#10个anchor
 
     im_info = im_info[0]#原始图像的高宽、缩放尺度
 
     assert rpn_cls_prob_reshape.shape[0] == 1, \
         'Only single item batches are supported'
-    pre_nms_topN  = cfg[cfg_key].RPN_PRE_NMS_TOP_N#12000,在做nms之前，最多保留的候选box数目
-    post_nms_topN = cfg[cfg_key].RPN_POST_NMS_TOP_N#2000，做完nms之后，最多保留的box的数目
-    nms_thresh    = cfg[cfg_key].RPN_NMS_THRESH#nms用参数，阈值是0.7
-    min_size      = cfg[cfg_key].RPN_MIN_SIZE#候选box的最小尺寸，目前是16，高宽均要大于16
+    pre_nms_topN  = cfg["TEST"]["RPN_PRE_NMS_TOP_N"]#12000,在做nms之前，最多保留的候选box数目
+    post_nms_topN = cfg["TEST"]["RPN_POST_NMS_TOP_N"]#2000，做完nms之后，最多保留的box的数目
+    nms_thresh    = cfg["TEST"]["RPN_NMS_THRESH"]#nms用参数，阈值是0.7
+    min_size      = cfg["TEST"]["RPN_MIN_SIZE"]#候选box的最小尺寸，目前是16，高宽均要大于16
 
     height, width = rpn_cls_prob_reshape.shape[1:3]#feature-map的高宽
 
@@ -77,25 +73,16 @@ def proposal_layer(rpn_cls_prob_reshape, rpn_bbox_pred, im_info, cfg_key, _feat_
     # 获取第一个分类结果
     scores = np.reshape(np.reshape(rpn_cls_prob_reshape, [1, height, width, _num_anchors, 2])[:,:,:,:,1],
                         [1, height, width, _num_anchors])
-    #提取到object的分数，non-object的我们不关心
-    #并reshape到1*H*W*10
+    # 提取到object的分数，non-object的我们不关心
+    # 并reshape到1*H*W*10
 
     bbox_deltas = rpn_bbox_pred#模型输出的pred是相对值，需要进一步处理成真实图像中的坐标
-    #im_info = bottom[2].data[0, :]
-
-    if DEBUG:
-        print('im_size: ({}, {})'.format(im_info[0], im_info[1]))
-        print('scale: {}'.format(im_info[2]))
-
-    # 1. Generate proposals from bbox deltas and shifted anchors
-    if DEBUG:
-        print('score map size: {}'.format(scores.shape))
+    # im_info = bottom[2].data[0, :]
 
     # Enumerate all shifts
     # 同anchor-target-layer-tf这个文件一样，生成anchor的shift，进一步得到整张图像上的所有anchor
     shift_x = np.arange(0, width) * _feat_stride
     shift_y = np.arange(0, height) * _feat_stride
-    #print('w,h,x',width,height,width*height)
 
     # shift_x shape = [height, width]
     # 生成同样维度的两个矩阵
@@ -105,7 +92,6 @@ def proposal_layer(rpn_cls_prob_reshape, rpn_bbox_pred, im_info, cfg_key, _feat_
     # shifts shape = [height*width,4]
     shifts = np.vstack((shift_x.ravel(), shift_y.ravel(),
                         shift_x.ravel(), shift_y.ravel())).transpose()
-    #print("shift shape", shifts.shape)
 
     # Enumerate all shifted anchors:
     #
@@ -117,10 +103,7 @@ def proposal_layer(rpn_cls_prob_reshape, rpn_bbox_pred, im_info, cfg_key, _feat_
     K = shifts.shape[0] # height*width,[height*width,4]
     anchors = _anchors.reshape((1, A, 4)) + \
               shifts.reshape((1, K, 4)).transpose((1, 0, 2))
-    # print('_anchors.reshape((1, A, 4))',np.shape(_anchors.reshape((1, A, 4))))
-    # print('shifts.reshape((1, K, 4)).transpose((1, 0, 2))',np.shape(shifts.reshape((1, K, 4)).transpose((1, 0, 2))))
     anchors = anchors.reshape((K * A, 4))#这里得到的anchor就是整张图像上的所有anchor
-    # print(anchors)
 
     # Transpose and reshape predicted bbox transformations to get them
     # into the same order as the anchors:
@@ -145,10 +128,9 @@ def proposal_layer(rpn_cls_prob_reshape, rpn_bbox_pred, im_info, cfg_key, _feat_
     keep = _filter_boxes(proposals, min_size * im_info[2])#移除那些proposal小于一定尺寸的proposal
     proposals = proposals[keep, :]#保留剩下的proposal
     scores = scores[keep]
-    bbox_deltas=bbox_deltas[keep,:]
+    bbox_deltas=bbox_deltas[keep, :]
 
-
-    # # remove irregular boxes, too fat too tall
+    # remove irregular boxes, too fat too tall
     # keep = _filter_irregular_boxes(proposals)
     # proposals = proposals[keep, :]
     # scores = scores[keep]
@@ -162,7 +144,6 @@ def proposal_layer(rpn_cls_prob_reshape, rpn_bbox_pred, im_info, cfg_key, _feat_
     scores = scores[order]
     bbox_deltas=bbox_deltas[order,:]
 
-
     # 6. apply nms (e.g. threshold = 0.7)
     # 7. take after_nms_topN (e.g. 300)
     # 8. return the top proposals (-> RoIs top)
@@ -172,7 +153,6 @@ def proposal_layer(rpn_cls_prob_reshape, rpn_bbox_pred, im_info, cfg_key, _feat_
     proposals = proposals[keep, :]
     scores = scores[keep]
     bbox_deltas=bbox_deltas[keep,:]
-
 
     # Output rois blob
     # Our RPN implementation only supports a single input image, so all
